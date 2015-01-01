ffff2d15554b1d127f39f5ce105f715111558d48
周培公 <peigong@foxmail.com>
2015-01-01 17:07:47 +0800

    修改构建

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -160,6 +160,7 @@ module.exports = function (grunt) {
         // The actual grunt server settings
         connect: {
             options: {
+                debug: true,
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
