b9692150fc95ed099b95e24ace76f49e1c2f3824
周培公 <peigong@foxmail.com>
2015-04-15 20:28:49 +0800

    ok

--- stock/stocks.js
+++
@@ -1,7 +0,0 @@
-{
-    "sh600036" : { "symbol" : "sh600036", "name" : "招商银行", "floor" : 9.75, "quantity" : 652,"cost" : 8.877 },
-    "sz000998" : { "symbol" : "sz000998", "name" : "隆平高科", "floor" : 16.33, "quantity" : 200,"cost" : 9.342 },
-    "sz000402" : { "symbol" : "sz000402", "name" : "金 融 街", "floor" : 6.41, "quantity" : 300,"cost" : 5.555 },
-    "sh600115" : { "symbol" : "sh600115", "name" : "东方航空", "floor" : 4.94, "quantity" : 900,"cost" : 1.295 },
-    "xj000001" : { "symbol" : "xj000001", "name" : "现金", "floor" : 0, "quantity" : 0,"cost" : 366.81 }
-}
