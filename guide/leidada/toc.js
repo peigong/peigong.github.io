5fdb9755d744935b7943fde72b7c87a2951f3e45
peigong <peigong@foxmail.com>
2026-09-23 22:23:33 +0800

    Site updated: 2026-09-23 22:22:42

--- guide/leidada/toc.js
+++ guide/leidada/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><div><strong aria-hidden="true">2.</strong> 2025</div></li><li><ol class="section"><li class="chapter-item expanded "><a href="2025/The-Temple-of-Heaven.html"><strong aria-hidden="true">2.1.</strong> The Temple of Heaven</a></li><li class="chapter-item expanded "><a href="2025/Q&A:The-Temple-of-Heaven.html"><strong aria-hidden="true">2.2.</strong> Q&amp;A: The temple of heaven</a></li></ol></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><div><strong aria-hidden="true">2.</strong> 2025</div></li><li><ol class="section"><li class="chapter-item expanded "><a href="2025/The-Temple-of-Heaven.html"><strong aria-hidden="true">2.1.</strong> The Temple of Heaven</a></li><li class="chapter-item expanded "><a href="2025/QA-The-Temple-of-Heaven.html"><strong aria-hidden="true">2.2.</strong> Q&amp;A: The temple of heaven</a></li></ol></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
